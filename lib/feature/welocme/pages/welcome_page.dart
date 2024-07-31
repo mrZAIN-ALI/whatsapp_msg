9e5e1cabdef8848793d415f75aeb29ed82c5a949
ZAIN ALI <97796695+mrZAIN-ALI@users.noreply.github.com>
2024-08-01 03:11:04 +0500

    SO i started working again

---
+++ lib/feature/welocme/pages/welcome_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Hello, World!',
+          
+        ),
+      ),
+    );
+  }
+}
